c18eff705c4ed6445eeff252e95246a5ce3a8de9
xnaas <me@xnaas.info>
2022-04-29 15:30:57 -0500

    [mcu] minor refactor

--- mcu.py
+++ mcu.py
@@ -1,9 +1,10 @@
 """
-Original author: xnaas (2021)
+Original author: xnaas (2021-2022)
 License: The Unlicense (public domain)
 """
 import requests
-from sopel import plugin, formatting
+from sopel import plugin
+from sopel.formatting import bold, italic
 
 
 @plugin.command("nextmcu")
@@ -16,15 +17,17 @@ def next_mcu(bot, trigger):
         days_until = str(r.json()["days_until"])
         media_title = r.json()["title"]
         media_type = r.json()["type"]
-        bot.say(
-            "Up next in the MCU is the {} '{}'. It will be out in {} days.".format(
-                media_type,
-                formatting.italic(media_title),
-                formatting.bold(days_until)))
+        msg = f"Up next in the MCU is the {media_type} '{italic(media_title)}'. "
+        msg += f"It will be out in {bold(days_until)} days."
+        bot.say(msg)
     except BaseException:
         bot.reply("Error reaching API, probably.")
 
 
+# TODO: This entire command is pretty fucked, tbh.
+#   Needs to have shit put into a dict or something
+#   and get some for loops going.
+#   - xnaas, 2022
 @plugin.command("next4mcu")
 @plugin.output_prefix("[MCU] ")
 def next4_mcu(bot, trigger):
@@ -58,13 +61,22 @@ def next4_mcu(bot, trigger):
         media_type_4 = r2.json()["following_production"]["type"]
 
         bot.say("Here are the next 4 upcoming MCU items:")
-        bot.say("Immediately up next is the {} '{}'. It will be out in {} days.".format(
-            media_type_1, formatting.italic(media_title_1), formatting.bold(days_until_1)))
-        bot.say("After that will be the {} '{}'. It will be out in {} days.".format(
-            media_type_2, formatting.italic(media_title_2), formatting.bold(days_until_2)))
-        bot.say("After that will be the {} '{}'. It will be out in {} days.".format(
-            media_type_3, formatting.italic(media_title_3), formatting.bold(days_until_3)))
-        bot.say("Finally, after that is the {} '{}'. It will be out in {} days.".format(
-            media_type_4, formatting.italic(media_title_4), formatting.bold(days_until_4)))
+
+        msg1 = f"Immediately up next is the {media_type_1} '{italic(media_title_1)}'. "
+        msg1 += f"It will be out in {bold(days_until_1)} days."
+
+        msg2 = f"After that will be the {media_type_2} '{italic(media_title_2)}'. "
+        msg2 += f"It will be out in {bold(days_until_2)} days."
+
+        msg3 = f"After that will be the {media_type_3} '{italic(media_title_3)}'. "
+        msg3 += f"It will be out in {bold(days_until_3)} days."
+
+        msg4 = f"Finally, after that is the {media_type_4} '{italic(media_title_4)}'. "
+        msg4 += f"It will be out in {bold(days_until_4)} days."
+
+        bot.say(msg1)
+        bot.say(msg2)
+        bot.say(msg3)
+        bot.say(msg4)
     except BaseException:
         bot.reply("Error reaching API, probably.")
