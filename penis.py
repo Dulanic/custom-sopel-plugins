226f5a6f5ce0a9c9e42a6d41f18c40b2cb8db9d3
xnaas <me@xnaas.info>
2022-04-29 22:45:58 -0500

    [penis] minor refactor

--- penis.py
+++ penis.py
@@ -1,20 +1,20 @@
 """
-Original author: xnaas (2021)
+Original author: xnaas (2021-2022)
 License: The Unlicense (public domain)
 """
 import random
 from sopel import plugin, tools
+from sopel.formatting import plain
 
 
 @plugin.command("penis")
 @plugin.require_chanmsg
 def literal_dick_measuring(bot, trigger):
     """100% accurate penis measuring."""
-    target = trigger.group(3) or trigger.nick
+    target = plain(trigger.group(3) or trigger.nick)
 
     if not target:
-        bot.reply("How in the hell did you do this?")
-        return
+        return bot.reply("How in the hell did you do this?")
 
     # Set Case Insensitivity
     target = tools.Identifier(target)
@@ -24,8 +24,7 @@ def literal_dick_measuring(bot, trigger):
 
     # Check user is in channel
     if target not in bot.channels[trigger.sender].users:
-        bot.reply("I need someone in chat to measure. ( ͡° ͜ʖ ͡°)")
-        return
+        return bot.reply("I need someone in chat to measure. ( ͡° ͜ʖ ͡°)")
 
     # Get dick length
     if target == bot.nick:
@@ -34,10 +33,10 @@ def literal_dick_measuring(bot, trigger):
         random.seed(str(target).lower())
         length = random.randint(0, 10)
 
-    dick_length = "8{}D".format("=" * length)
+    dick_length = f"8{'=' * length}D"
 
     # Restore random state
     random.setstate(state)
 
     # Tell user their dick length
-    bot.say("{}'s dick size: {}".format(target, dick_length))
+    bot.say(f"{target}'s dick size: {dick_length}")
