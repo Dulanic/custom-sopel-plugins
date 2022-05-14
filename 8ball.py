2b443bdf34d96774025fdf3632f33e11b9d04892
xnaas <me@xnaas.info>
2022-05-13 22:46:36 -0500

    [8ball] " → '

--- 8ball.py
+++ 8ball.py
@@ -11,31 +11,31 @@ from sopel import plugin
 def eightball(bot, trigger):
     """The magic 8ball is all-knowing and 100% accurate."""
     if not trigger.group(2):
-        return bot.say(f"I need something to foretell, {trigger.nick}!")
+        return bot.say(f'I need something to foretell, {trigger.nick}!')
 
     messages = [
         # Positive Replies (10)
-        "It is certain.",
-        "It is decidedly so.",
-        "Without a doubt.",
-        "Yes – definitely.",
-        "You may rely on it.",
-        "As I see it, yes.",
-        "Most likely.",
-        "Outlook good.",
-        "Yes.",
-        "Signs point to yes.",
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a doubt.',
+        'Yes – definitely.',
+        'You may rely on it.',
+        'As I see it, yes.',
+        'Most likely.',
+        'Outlook good.',
+        'Yes.',
+        'Signs point to yes.',
         # Ambiguous Bullshit Replies (5)
-        "Reply hazy, try again.",
-        "Ask again later.",
-        "Better not tell you now.",
-        "Cannot predict now.",
-        "Concentrate and ask again.",
+        'Reply hazy, try again.',
+        'Ask again later.',
+        'Better not tell you now.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
         # Negative Replies (5)
         "Don't count on it.",
-        "My reply is no.",
-        "My sources say no.",
-        "Outlook not so good.",
-        "Very doubtful."
+        'My reply is no.',
+        'My sources say no.',
+        'Outlook not so good.',
+        'Very doubtful.'
     ]
     bot.reply(choice(messages))
