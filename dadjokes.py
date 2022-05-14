93c8d1a7a54ed1acaffff9a20205cbcf7b8492dc
xnaas <me@xnaas.info>
2022-05-14 00:00:34 -0500

    [dadjokes] correct Exception

--- dadjokes.py
+++ dadjokes.py
@@ -6,13 +6,13 @@ import requests
 from sopel import plugin
 
 
-@plugin.commands("dadjoke", "dj")
+@plugin.commands('dadjoke', 'dj')
 def dadjoke(bot, trigger):
     """Posts a dad joke."""
-    url = "https://icanhazdadjoke.com/"
-    headers = {"Accept": "application/json"}
+    url = "https://icanhazdadjoke.com"
+    headers = {'Accept': 'application/json'}
     try:
         dad_joke = requests.get(url, headers=headers).json()['joke']
-        bot.say(dad_joke)
-    except:
-        bot.reply("Error reaching API, probably.")
+    except Exception:
+        return bot.reply("Error reaching API, probably.")
+    bot.say(dad_joke)
